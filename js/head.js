f8c8d49b08bb56cc077b5bfe2be3d43d1b40c54d
Fernando <fernandomgp17@gmail.com>
2022-07-03 19:08:49 -0500

    Termino de header

--- js/head.js
+++ js/head.js
@@ -1,3 +1,9 @@
-<script>
-  AOS.init();
-</script>
+//Inicializamos AOS librery
+AOS.init();
+
+const menu = document.getElementById('menu');
+const menu_bar = document.getElementById('menu-bar');
+
+menu_bar.addEventListener('click',function() {
+    menu.classList.toggle('menu-toggle');
+});
